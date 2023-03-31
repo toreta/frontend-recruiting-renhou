2cbe6bb77a7f131167ee22d6c03f08d9308a6807
kensoz <kensozlian@gmail.com>
2023-03-31 22:26:56 +0900

    fix: assignment-1

--- assignment-1/src/charge.ts
+++ assignment-1/src/charge.ts
@@ -1,50 +1,54 @@
-export type Invoice = {
-  total: number;
-};
+// * ------------------------------
+// *
+// * charge.ts
+// * 請求書(Invoice)と支払い(Paymentes)を受け取り、レシート(Receipt)を返却します。
+// * https://github.com/toreta/frontend-recruiting-renhou/tree/main/assignment-1
+// *
+// * ------------------------------
 
-export type Receipt = {
-  total: number;
-  deposit: number;
-  change: number;
-};
+import type { Invoice, Payment, Receipt } from './charge.type';
 
-export type Payment = {
-  type: string;
-  percentage?: number;
-  amount?: number;
-};
-
-export function charge(invoice: Invoice, payments: Payment[]) {
-  const total = invoice.total;
+/**
+ * 請求書と支払い情報を元にレシートを生成する関数
+ * @param {Invoice} invoice - 請求書
+ * @param {Payment[]} payments - 支払い情報の配列
+ * @returns {Receipt} receipt - レシート
+ * @throws {Error} 'Shortage' - 支払いが不足している場合
+ * @throws {Error} 'OverCharge' - クーポンで全額支払いが可能な場合に現金も含まれている場合
+ */
+const charge = (invoice: Invoice, payments: Payment[]): Receipt => {
+  // 変数、定数定義
+  const total: number = invoice.total;
   let deposit = 0;
+  let couponPayment = 0;
 
-  payments
-    .sort((payment) => (payment.type !== 'CASH' ? -1 : 1))
-    .map((payment) => {
-      if (payment.type === 'COUPON') {
-        if (payment.percentage != null) {
-          deposit += Math.floor(total * (payment.percentage / 100));
-        } else {
-          deposit += payment.amount || 0;
-        }
-      } else {
-        if (deposit >= total) {
-          throw new Error('OverCharge');
-        }
-        deposit += payment.amount || 0;
-      }
-    });
-  if (total > deposit) {
+  // 各支払い情報に対して、クーポンの場合はcouponPaymentに、現金はdepositに加算する
+  payments.forEach((payment: Payment): void => {
+    if (payment.type === 'COUPON') {
+      couponPayment += payment.percentage
+        ? Math.floor(total * (payment.percentage / 100))
+        : payment.amount || 0;
+    } else {
+      deposit += payment.amount || 0;
+    }
+  });
+
+  // 支払いが請求額を下回る場合はエラー
+  if (total > deposit + couponPayment) {
     throw new Error('Shortage');
   }
 
-  let isCoupon = true;
-  for (let i = 0; i < payments.length; i++) {
-    if (payments[i].type !== 'COUPON') {
-      isCoupon = false;
-      continue;
-    }
+  // 全ての支払いがクーポンであるかどうかを判定する
+  const isCoupon = payments.every((payment: Payment): boolean => payment.type === 'COUPON');
+  // クーポンで支払った場合、お釣りは0。それ以外の場合、お釣りは支払い額から請求額を引く
+  const change: number = isCoupon ? 0 : deposit + couponPayment - total;
+
+  // クーポンで全額支払いが可能な場合に、現金も含まれている場合はエラーを投げる
+  if (!isCoupon && couponPayment >= total) {
+    throw new Error('OverCharge');
   }
-  if (isCoupon) return { total, deposit, change: 0 };
-  return { total: total, deposit: deposit, change: deposit - total };
-}
+
+  return { total, deposit: deposit + couponPayment, change };
+};
+
+export { charge };
